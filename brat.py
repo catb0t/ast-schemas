f5463004f4ff8920ec710c0f061cf325d90efbf8
Cat Stevens <catb0t@protonmail.ch>
2019-10-24 22:02:12 -0400

    a lot of new features and fixed bugs

--- brat.py
+++ brat.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python3
 import json
+import sys
 import pprint
 import jsonschema
 
@@ -20,12 +21,35 @@ def pprint_jscexc (ex):
     })
 
 
+def must_fails(schema):  # noqa
+    must_fail_file = "brat_must_fail.json"
+    with open(must_fail_file) as fp:
+        must_fail = json.load(fp)
+    count_progs = 0
+    count_failed = 0
+    for node in must_fail:
+        if isinstance(node, list):
+            count_progs += 1
+            for prog in node:
+                try:
+                    jsonschema.validate(instance=prog, schema=schema)
+                    print("\tFAIL", prog)
+                    count_failed += 1
+                except jsonschema.exceptions.ValidationError:
+                    print("\tPASS")
+        elif isinstance(node, dict) and node["_comment"]:
+            print(node["_note"])
+        else:
+            raise ValueError(node)
+    print(f"1 + {count_progs} tests, {count_failed} failures")
+
+
 def main():
     schema_file = "brat.schema.json"
-    instance_file = "brat_test.json"  # "brat_test.json"
+    good_instance_file = "brat_test.json"
     with open(schema_file, "r") as fp:
         schema = json.load(fp)
-    with open(instance_file, "r") as fp:
+    with open(good_instance_file, "r") as fp:
         instance = json.load(fp)
 
     try:
@@ -33,10 +57,13 @@ def main():
     except jsonschema.exceptions.ValidationError as ex:
         print("bad INSTANCE")
         pprint_jscexc(ex)
-
+        sys.exit(2)
     except jsonschema.exceptions.SchemaError as ex:
         print("bad SCHEMA")
         pprint_jscexc(ex)
+        sys.exit(2)
+
+    must_fails(schema)
 
 
 if __name__ == '__main__':
